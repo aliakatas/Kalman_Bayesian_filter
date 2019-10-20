85a427b2372fa7479abae459b8dc92847bddf0bf
aliakatas <aristotelis.liakatas@gmail.com>
2019-10-20 21:54:33 +0100

    Improving test script with functionality. Adding lognormal case and place-holder for weibull.

--- Bayes/Bayes.py
+++ Bayes/Bayes.py
@@ -5,6 +5,26 @@ combination with the Kalman filter.
 """
 
 import numpy as np
+from scipy import stats
+import scipy.integrate as integrate
+
+def lognormalIntegralNumerator(x, model, varErr, mu, sigma):
+    """
+    Function used in the numerator of the integral
+    performing the correction.
+    """
+    a1 = ((model - x) ** 2) / (2. * varErr ** 2)
+    b1 = ((np.log(x) - mu) ** 2) / (2 * sigma ** 2)
+    return np.exp(-a1 - b1)
+
+def lognormalIntegralDenominator(x, model, varErr, mu, sigma):
+    """
+    Function used in the denominator of the integral
+    performing the correction.
+    """
+    a1 = ((model - x) ** 2) / (2. * varErr ** 2)
+    b1 = ((np.log(x) - mu) ** 2) / (2 * sigma ** 2)
+    return np.exp(-a1 - b1) / x
 
 class Bayes:
     def __init__(self, history):
@@ -17,10 +37,22 @@ class Bayes:
         self.modValues = np.zeros(history)      # Initialise model results storage
 
         self.correctionType = 'none'            # Initialise type of data to be handled/corrected (Normal Dist)
+        self.maxData = 0.0                      # Maximum value to be encountered in data
+        self.minData = 0.0                      # Minimum value to be encountered in data
+        self.integrInterv = 1000                # Number of intervals to consider when integrating numerically (Weibull or Lognormal)
+        
+        # Normal dist related characteristics
         self.avgObs = None                      # Average of values of observations from history (Normal Dist)
         self.varObs = None                      # Variance of values of observations from history (Normal Dist)
-        self.varError = None                    # Variance of error between observations and model results (Normal Dist)
+        self.varError = None                    # Variance of error between observations and model results (Normal and Lognormal Dist)
         self.varCorrection = None               # Variance value to be used for correction (Normal Dist)
+
+        # Lognormal dist related characterisics
+        self.mu = None                          # Mean value of observations from history (based on lognormal dist)
+        self.sigma = None                       # Std dev value of observations from history (based on lognormal dist)
+        
+        # Weibull dist related characteristics
+
 
     def trainMe(self, obs, model, distrType = None):
         """
@@ -45,11 +77,65 @@ class Bayes:
             self.correctionType = distrType
 
         # Hardwire normal for now...
-        self.avgObs = np.mean(myObs)
-        self.varObs = np.var(myObs)
-        self.varError = np.var(myModel - myObs)
+        for ij in range(NN):
+            self.updateHistory(myObs[ij], myModel[ij])
+
+        self.updateCoefficientsNormal()
+
+    def updateHistory(self, obs, model):
+        """
+        Update values stored as history data.
+        """
+        self.obsValues[1:] = self.obsValues[0:-1]
+        self.modValues[1:] = self.modValues[0:-1]
+
+        self.obsValues[0] = obs
+        self.modValues[0] = model
+
+    def updateCoefficientsNormal(self):
+        """
+        Given the history data, update the coefficients
+        for normal distribution corrections.
+        """
+        self.avgObs = np.mean(self.obsValues)
+        self.varObs = np.var(self.obsValues)
+        self.varError = np.var(self.modValues - self.obsValues)
         self.varCorrection = 1. / ((1./self.varError) + (1./self.varObs))
 
-        
+    def updateCoefficientsLognormal(self):
+        """
+        Given the history data, update the coefficients
+        for lognormal distribution corrections.
+        """
+        self.varError = np.var(self.modValues - self.obsValues)
+        tempObs = self.obsValues[self.obsValues > 0.0]
+
+        shape, loc, scale = stats.lognorm.fit(tempObs, floc=0)
+        self.mu = np.log(scale)
+        self.sigma = shape
+
+    def correctValueNormal(self, pred):
+        """
+        Provides correction for the prediction pred,
+        based on internal (history) values. (Normal dist)
+        """
+        return ((1./self.varError) * pred + (1./self.varObs) * self.avgObs) * self.varCorrection
+
+    def correctValueLognormal(self, pred):
+        """
+        Provides correction for the prediction pred,
+        based on internal (history) values. (Lognormal dist)
+        """
+        valNumerator = integrate.quad(lambda x: lognormalIntegralNumerator(x, pred, self.varError, self.mu, self.sigma), self.minData, self.maxData)
+        valDenominator = integrate.quad(lambda x: lognormalIntegralDenominator(x, pred, self.varError, self.mu, self.sigma), self.minData, self.maxData)
+        return valNumerator / valDenominator
+
+    def correctValueWeibull(self, pred):
+        """
+        Provides correction for the prediction pred,
+        based on internal (history) values. (Weibull dist)
+        """
+        pass
+
 
 
--- Bayes/testScript.py
+++ Bayes/testScript.py
@@ -4,10 +4,32 @@ from Bayes import Bayes
 if __name__ == '__main__':
 
     # Define some generic values
-    history = 7
+    history = 10
 
     # Create some imaginary data
     obsNormal = np.random.normal(15.4, 2.3, size = 2000)
     modelNormal = obsNormal + np.random.normal(2.5, 0.5, size = len(obsNormal))
+
+    # Define the plot range and size of the figure
+    pltRange = range(1000, 1045)
+    figSize = (11,9)
+
+    # Start Testing
+    # Create an instance of the object
+    bs = Bayes(history)
+
+    # Use the first #history of them for training
+    obs_train = obsNormal[:history]
+    model_train = modelNormal[:history]
+
+    # The rest to be used dynamically
+    obs_dyn = obsNormal[history:]
+    model_dyn = modelNormal[history:]
+    fcst = np.zeros_like(obs_dyn)
+
+    # Perform an initial training of the model
+    bs.trainMe(obs_train, model_train)
+    
+
 
     
