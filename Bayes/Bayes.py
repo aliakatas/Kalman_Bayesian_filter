bd75e15214921c32df67ddad4afa0e28417dbf28
aliakatas <aristotelis.liakatas@gmail.com>
2019-10-21 11:46:41 +0100

    Added blocks of tests for each supported distribution. Workflow within class completed. Covering from initialisation, to training and correcting values.

--- Bayes/Bayes.py
+++ Bayes/Bayes.py
@@ -9,6 +9,20 @@ from scipy import stats
 import scipy.integrate as integrate
 import scipy.stats as stats
 
+# Names of distributions currently supported by the system 
+distNames = ['norm', 'lognorm', 'weibull_min']
+distAllowed = {
+    'norm':'norm', 
+    'lognorm':'lognorm', 
+    'weibull_min':'weibull_min', 
+    'normal':'norm', 
+    'lognormal':'lognorm', 
+    'log':'lognorm', 
+    'weibull':'weibull_min', 
+    'wei':'weibull_min', 
+    'weib':'weibull_min'
+}
+
 def lognormalIntegralNumerator(x, model, varErr, mu, sigma):
     """
     Function used in the numerator of the integral
@@ -27,62 +41,85 @@ def lognormalIntegralDenominator(x, model, varErr, mu, sigma):
     b1 = ((np.log(x) - mu) ** 2) / (2 * sigma ** 2)
     return np.exp(-a1 - b1) / x
 
+def weibullIntegralNumerator(x, model, varErr, shape, scale):
+    """
+    Function used in the numerator of the integral
+    performing the correction.
+    """
+    return (x ** shape) * np.exp(-(x/scale) ** shape) * np.exp((-(model - x) ** 2) / (2. * varErr))
+
+def weibullIntegralDenominator(x, model, varErr, shape, scale):
+    """
+    Function used in the denominator of the integral
+    performing the correction.
+    """
+    return (x ** (shape - 1.)) * np.exp(-(x/scale) ** shape) * np.exp((-(model - x) ** 2) / (2. * varErr))
+
 def findBestDistribution(data):
     """
     Perform some tests to determine the distribution
     that best fits the data.
     """
-    dist_names = ["norm", "lognorm", "weibull_max", "weibull_min"]
     dist_results = []
     params = {}
-    for dist_name in dist_names:
+    for dist_name in distNames:
         dist = getattr(stats, dist_name)
         param = dist.fit(data)
 
         params[dist_name] = param
         # Applying the Kolmogorov-Smirnov test
         D, p = stats.kstest(data, dist_name, args=param)
-        print("p value for "+dist_name+" = "+str(p))
+        #print("p value for "+dist_name+" = "+str(p))
         dist_results.append((dist_name, p))
 
     # select the best fitted distribution
     best_dist, best_p = (max(dist_results, key=lambda item: item[1]))
     # store the name of the best fit and its p value
 
-    print("Best fitting distribution: "+str(best_dist))
-    print("Best p value: "+ str(best_p))
-    print("Parameters for the best fit: "+ str(params[best_dist]))
+    # print("Best fitting distribution: "+str(best_dist))
+    # print("Best p value: "+ str(best_p))
+    # print("Parameters for the best fit: "+ str(params[best_dist]))
     return best_dist, best_p, params[best_dist]
 
 class Bayes:
-    def __init__(self, history):
+    def __init__(self, history, distType = None):
+        # Check for invalid length of history data
         if history < 1:
             raise ValueError('History length is too small')
+        
+        # Check if the distribution is supported
+        if distType is None:
+            self.correctionType = 'none'            # Initialise type of data to be handled/corrected (will be detected)
+        else:
+            if distType.lower() in distAllowed.keys():
+                self.correctionType = distAllowed[distType.lower()] # Initialise type of data to be handled/corrected (user defined)
+            else:
+                raise ValueError('Distribution not currently implemented for the system.')
 
         # Set the bare minimum info needed
         self.history = history                  # History length
         self.obsValues = np.zeros(history)      # Initialise observations storage
         self.modValues = np.zeros(history)      # Initialise model results storage
 
-        self.correctionType = 'none'            # Initialise type of data to be handled/corrected (Normal Dist)
         self.maxData = 0.0                      # Maximum value to be encountered in data
-        self.minData = 0.0                      # Minimum value to be encountered in data
+        self.minData = 0.0001                   # Minimum value to be encountered in data
         self.integrInterv = 1000                # Number of intervals to consider when integrating numerically (Weibull or Lognormal)
         
         # Normal dist related characteristics
         self.avgObs = None                      # Average of values of observations from history (Normal Dist)
         self.varObs = None                      # Variance of values of observations from history (Normal Dist)
-        self.varError = None                    # Variance of error between observations and model results (Normal and Lognormal Dist)
         self.varCorrection = None               # Variance value to be used for correction (Normal Dist)
+        self.varError = None                    # Variance of error between observations and model results (Normal, Lognormal and Weibull Dists)
 
         # Lognormal dist related characterisics
-        self.mu = None                          # Mean value of observations from history (based on lognormal dist)
-        self.sigma = None                       # Std dev value of observations from history (based on lognormal dist)
+        self.mu = None                          # Mean value of observations from history (based on Lognormal Dist)
+        self.sigma = None                       # Std dev value of observations from history (based on Lognormal Dist)
         
         # Weibull dist related characteristics
+        self.scale = None                       # Scale parameter value of observations from history (based on Weibull Dist) 
+        self.shape = None                       # Shape parameter value of observations from history (based on Weibull Dist) 
 
-
-    def trainMe(self, obs, model, distrType = None):
+    def trainMe(self, obs, model, retarget = False):
         """
         Master method to control the 
         initial training of the system.
@@ -95,21 +132,32 @@ class Bayes:
         if myObs.shape != myModel.shape:
             raise TypeError('Initial training set does not have conforming shapes.')
 
+        # Update object's database
         NN = len(myObs)
-
-        # TODO
-        # Could add a method to auto-detect distribution...
-        if distrType is None:
-            self.correctionType = 'something'       # temporary for development
-        else:
-            self.correctionType = distrType
-
-        # Hardwire normal for now...
         for ij in range(NN):
             self.updateHistory(myObs[ij], myModel[ij])
 
-        self.updateCoefficientsNormal()
+        # Check if the user would like to find best fit each time...
+        if retarget:
+           self.correctionType == 'none'
 
+        # Check what distribution we are dealing with
+        if self.correctionType == 'none':
+            # Must detect it...
+            findBestDistribution(myObs)
+        
+        # Perform update of coefficients
+        if self.correctionType == 'norm':
+            self.updateCoefficientsNormal()
+        elif self.correctionType == 'lognorm':
+            self.updateCoefficientsLognormal()
+        elif self.correctionType == 'weibull_min':
+            self.updateCoefficientsWeibull()
+
+        # Update the maximum data value (estimate)
+        mx = max(myObs)
+        self.maxData = max([self.maxData, 10.0 * mx])
+            
     def updateHistory(self, obs, model):
         """
         Update values stored as history data.
@@ -129,6 +177,7 @@ class Bayes:
         self.varObs = np.var(self.obsValues)
         self.varError = np.var(self.modValues - self.obsValues)
         self.varCorrection = 1. / ((1./self.varError) + (1./self.varObs))
+        return 
 
     def updateCoefficientsLognormal(self):
         """
@@ -138,9 +187,23 @@ class Bayes:
         self.varError = np.var(self.modValues - self.obsValues)
         tempObs = self.obsValues[self.obsValues > 0.0]
 
-        shape, loc, scale = stats.lognorm.fit(tempObs, floc=0)
+        shape, _, scale = stats.lognorm.fit(tempObs, floc=0)
         self.mu = np.log(scale)
         self.sigma = shape
+        return 
+    
+    def updateCoefficientsWeibull(self):
+        """
+        Given the history data, update the coefficients
+        for weibull distribution corrections.
+        """
+        self.varError = np.var(self.modValues - self.obsValues)
+        tempObs = self.obsValues[self.obsValues > 0.0]
+
+        shape, _, scale = stats.weibull_min.fit(tempObs, loc = 0)
+        self.shape = shape 
+        self.scale = scale
+        return 
 
     def correctValueNormal(self, pred):
         """
@@ -156,14 +219,31 @@ class Bayes:
         """
         valNumerator = integrate.quad(lambda x: lognormalIntegralNumerator(x, pred, self.varError, self.mu, self.sigma), self.minData, self.maxData)
         valDenominator = integrate.quad(lambda x: lognormalIntegralDenominator(x, pred, self.varError, self.mu, self.sigma), self.minData, self.maxData)
-        return valNumerator / valDenominator
+        return valNumerator[0] / valDenominator[0]
 
     def correctValueWeibull(self, pred):
         """
         Provides correction for the prediction pred,
         based on internal (history) values. (Weibull dist)
         """
-        pass
+        valNumerator = integrate.quad(lambda x: weibullIntegralNumerator(x, pred, self.varError, self.shape, self.scale), self.minData, self.maxData)
+        valDenominator = integrate.quad(lambda x: weibullIntegralDenominator(x, pred, self.varError, self.shape, self.scale), self.minData, self.maxData)
+        return valNumerator[0] / valDenominator[0]
+
+    def adjust_forecast(self, model):
+        """
+        Method to control the correction
+        of the forecast value.
+        """
+        if self.correctionType == 'norm':
+            return self.correctValueNormal(model)
+        elif self.correctionType == 'lognorm':
+            return self.correctValueLognormal(model)
+        elif self.correctionType == 'weibull_min':
+            return self.correctValueWeibull(model)
+        else:
+            print('Requested dist: {}'.format(self.correctionType))
+            raise TypeError('Unknown distribution type...')
 
 
 
--- Bayes/testScript.py
+++ Bayes/testScript.py
@@ -1,41 +1,126 @@
 import numpy as np
+from scipy.stats import weibull_min
 from Bayes import Bayes
+from matplotlib import pyplot as plt
+
+#testDists = ['norm', 'weibull_min', 'lognorm']
+testDists = ['weibull_min']
+
+def testNormal(hLen, totN, figSize, pltRange):
+    """
+    Brief test with data following normal distribution.
+    """
+    # Create some imaginary data
+    obs = np.random.normal(15.4, 1.3, size = totN)
+    model = obs + np.random.normal(0.0, 4.5, size = len(obs))
+
+    # Start Testing
+    # Create an instance of the object
+    bs = Bayes(hLen, distType = 'norm')
+
+    # Use the first #hLen of them for training
+    obsTrain = obs[:hLen]
+    modelTrain = model[:hLen]
+
+    # The rest to be used dynamically
+    obsDyn = obs[hLen:]
+    modelDyn = model[hLen:]
+    fcst = np.zeros_like(obsDyn)
+
+    # Perform an initial training of the model
+    bs.trainMe(obsTrain, modelTrain)
+
+    for ij in range(len(obsDyn)):
+        # Provide a correction to the forecast
+        fcst[ij] = bs.adjust_forecast(modelDyn[ij])
+
+        # Update system
+        bs.trainMe([obsDyn[ij]], [modelDyn[ij]])
+    
+    plt.figure(figsize = figSize)
+    plt.plot(obsDyn[pltRange], label='obs')
+    plt.plot(modelDyn[pltRange], label='model')
+    plt.plot(fcst[pltRange], '*', label='Bayes')
+    plt.title('Normal Dist Data')
+    plt.legend()
+    plt.show()
+    return
+
+def testWeibull(hLen, totN, figSize, pltRange):
+    """
+    Brief test with data following weibull distribution.
+    """
+    # Create some imaginary data
+    obs = weibull_min.rvs(c = 1.96, scale = 2.1, size = totN) # c: shape
+    model = obs + np.random.normal(0.0, 2.5, size = len(obs))
+
+    # Start Testing
+    # Create an instance of the object
+    bs = Bayes(hLen, distType = 'weibull_min')
+
+    # Use the first #hLen of them for training
+    obsTrain = obs[:hLen]
+    modelTrain = model[:hLen]
+
+    # The rest to be used dynamically
+    obsDyn = obs[hLen:]
+    modelDyn = model[hLen:]
+    fcst = np.zeros_like(obsDyn)
+
+    # Perform an initial training of the model
+    bs.trainMe(obsTrain, modelTrain)
+
+    for ij in range(len(obsDyn)):
+        # Provide a correction to the forecast
+        fcst[ij] = bs.adjust_forecast(modelDyn[ij])
+
+        # Update system
+        bs.trainMe([obsDyn[ij]], [modelDyn[ij]])
+
+    plt.figure(figsize = figSize)
+    plt.plot(obsDyn[pltRange], label='obs')
+    plt.plot(modelDyn[pltRange], label='model')
+    plt.plot(fcst[pltRange], '*', label='Bayes')
+    plt.title('Weibull Dist Data')
+    plt.legend()
+    plt.show()
+    return
+
+def testLognormal():
+    """
+    Brief test with data following lognormal distribution.
+    """
+    pass
 
 if __name__ == '__main__':
 
     # Define some generic values
-    history = 10
-
-    # Create some imaginary data
-    obsNormal = np.random.normal(15.4, 2.3, size = 2000)
-    modelNormal = obsNormal + np.random.normal(2.5, 0.5, size = len(obsNormal))
+    history = 100
+    totalNum = 2000
+    plotNum = 40
 
     # Define the plot range and size of the figure
-    pltRange = range(1000, 1045)
-    figSize = (11,9)
+    figSize = (11, 9)
+    pIn = int(totalNum * .5)
+    pltRange = range(pIn, pIn + plotNum)
+    
+    for item in testDists:
+        if item.lower() == 'norm':
+            print('\n *****   Normal Distribution data testing    ***** ')
+            testNormal(history, totalNum, figSize, pltRange)
+            print('\n ***** Normal Distribution data testing  END ***** ')
+        elif item.lower() == 'weibull_min':
+            print('\n *****   Weibull Distribution data testing    ***** ')
+            testWeibull(history, totalNum, figSize, pltRange)
+            print('\n ***** Weibull Distribution data testing  END ***** ')
+        elif item.lower() == 'lognorm':
+            print('\n *****   Lognormal Distribution data testing    ***** ')
+            testLognormal()
+            print('\n ***** Lognormal Distribution data testing  END ***** ')
+        else:
+            raise TypeError('Unknown distribution used for testing...')
 
-    # Start Testing
-    # Create an instance of the object
-    bs = Bayes(history)
-
-    # Use the first #history of them for training
-    obs_train = obsNormal[:history]
-    model_train = modelNormal[:history]
-
-    # The rest to be used dynamically
-    obs_dyn = obsNormal[history:]
-    model_dyn = modelNormal[history:]
-    fcst = np.zeros_like(obs_dyn)
-
-    # Perform an initial training of the model
-    bs.trainMe(obs_train, model_train)
-
-    for ij in range(len(obs_dyn)):
-        # Provide a correction to the forecast
-        #fcst[ij] = ###kf.adjust_forecast(model_dyn[ij])
-
-        # Update system
-        bs.trainMe([obs_dyn[ij]], [model_dyn[ij]])
+    
     
 
 
